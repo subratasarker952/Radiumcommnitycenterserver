1fe2ac71cb77fcc1be69e473ca11e53f6d76cf08
Subrata Sarkar <tccdcc8@gmail.com>
2024-06-14 19:25:13 +0600

    m

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT ;
 
 
 app.use(express.json());
